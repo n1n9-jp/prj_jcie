5edcbfab56cbaf865227bb768d9a04d04a885341
Yuichi Yazaki <visualizingjp@gmail.com>
2025-11-30 18:14:42 +0900

    ビルドファイル

--- docs/assets/malariae-BepXedHy.js
+++
@@ -1,1 +0,0 @@
-import"./navigation-manager-BG1z30ep.js";import{S as e}from"./ScrollytellingApp-CTAG7f4j.js";window.DISEASE_TYPE="malariae";document.addEventListener("DOMContentLoaded",()=>{window.app=new e});
